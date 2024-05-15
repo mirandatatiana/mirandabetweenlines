2f405279c6c1abe18e9a73f0edbd471e13b65d75
tati <miranda.tatianayamila@gmail.com>
2024-05-15 22:05:56 +0200

    change in build

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,9 +8,9 @@ const NavBar = () => {
 
 <nav class="bg-white border-gray-200 bg-gradient-to-l from-indigo-400 to-indigo-950">
   <div class=" flex  items-center justify-between w-full  p-4">
-    <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
+    <h1 href="" class="flex items-center space-x-3 rtl:space-x-reverse">
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MirandaBetweenLines</span>
-    </a>
+    </h1>
 
   </div>
 </nav>
--- src/components/workwithme.jsx
+++ src/components/workwithme.jsx
@@ -1,5 +1,4 @@
 
-import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
 
 const Workwithme = () => {
 return(
