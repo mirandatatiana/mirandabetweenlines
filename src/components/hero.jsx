afeeae3aac7a680653e9d93184e948dcb93520a1
tati <miranda.tatianayamila@gmail.com>
2024-05-16 21:50:14 +0200

    style change for mobile

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -7,11 +7,11 @@ const Hero = () => {
             <h2 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Hello There</h2>
             <h1 class="max-w-2xl mb-6 font-extrabold  text-4xl dark:text-white">I’m Tatiana a Front-end developer</h1>
             <h2 class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-            Actually a I’m a creative person. One how love coding
+            Actually a I’m a creative person. One how love to code
                             <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </h2>
             <a href="assets/TatianaMirandaCV.pdf" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-               CV
+               Resume
             </a> 
         </div>
         <div class="hidden border-opacity-55 lg:mt-0 lg:col-span-5 lg:flex">
